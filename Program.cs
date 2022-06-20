f4df90047d557d5a0683820e96f1ab35c14a0690
ilkin1zahidov <ilkiniz@code.edu.az>
2022-06-21 02:13:35 +0400

    find the methods

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //nums(21);
+
+            //task2
+            //nums(12, 18);
+
+            //task3
+            //numsSingle(5, 10);
+           
+        }
+        //task1
+        //Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+        //static void nums(int n) 
+        //{ 
+        //     if (n % 2 == 1) 
+        //     {
+        //        Console.WriteLine("3-e ve 7-e bolunur");   
+
+
+        //     }
+        //    else 
+        //    {
+        //        Console.WriteLine("bolunmur");
+        //    }
+
+        //task2
+        //n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+        //static void nums(int n, int m)
+        //{ 
+        // if (n % 2 == 0 && m % 2 == 0) 
+        // {
+        //        Console.WriteLine(n + m);
+
+        // } 
+        // else 
+        // {
+        //        Console.WriteLine("n ve m cut deyil");      
+        // }
+
+
+        //}
+
+        //task3
+        //Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+        //static void numsSingle(int n , int m, int sum = 0) 
+        //{
+        //    for (int i=n; i <= m; i++)
+
+        //        if (i % 2 == 1)
+        //            sum += i;
+        //    Console.WriteLine(sum);
+        //
+
+        
+        
+
+        
+        
+            
+            
+                
+
+                     
+                    
+                        
+                    
+                      
+                     
+                       
+                     
+                      
+                      
+                       
+                              
+
+            
+        
+    }
+
+
+
+
+
+}
+
+
+
+
+    
+
